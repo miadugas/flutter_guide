5011012cc609c7fa796dd478a7cf973662796ebe
Mia Dugas <miadugas@outlook.com>
2020-07-13 22:47:33 -0600

    flutter quiz guide

---
+++ lib/result.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are just so innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore <= 16) {
+      resultText = 'Ohh there\'s some darkness in there!';
+    } else {
+      resultText = 'Hail your darkness!';
+    }
+
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          RaisedButton(
+            child: Text(
+              'Restart Quiz!',
+              ),
+              textColor: Colors.blue,
+            onPressed: resetHandler,
+          )
+        ],
+      ),
+    );
+  }
+}
